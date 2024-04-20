b67f529aa7355cbdf422e51e812be3f59f58e079
Shikhar <shikharsharma612@gmail.com>
2024-04-20 18:37:05 +0530

    code update

--- blocks/featured/featured.js
+++ blocks/featured/featured.js
@@ -10,12 +10,35 @@ export default function decorate(block){
 
     // Create div element with class "row" and "no-gutters"
     const row2 = document.createElement('div');
-    row1.classList.add('row', 'no-gutters', 'mt-sm-3');
+    row2.classList.add('row', 'no-gutters', 'mt-sm-3');
+
+    interestsMain.appendChild(row1);
+    interestsMain.appendChild(row2);
     
-    [...block.children].forEach((row)=>{
-        console.log(row);
+    [...block.children].forEach((row,index)=>{
+        if(index==0){
+            const firstImgDiv = document.createElement('div');
+            firstImgDiv.classList.add('col-lg-6', 'mb-15', 'pr-0', 'pr-lg-3')
+            
+            // Create insights wrapper div with classes "insights-wraper", "home-overlay", and "insights-hover"
+            const insightsWrapper1 = document.createElement('div');
+            insightsWrapper1.classList.add('insights-wraper', 'home-overlay', 'insights-hover');
+            insightsWrapper1.style.height = '282.45px';
+
+            const imgElement = document.createElement('img');
+            imgElement.setAttribute('src', col.querySelector('img').getAttribute('src'));
+            imgElement.setAttribute('alt', 'Use Generative AI with Responsibility');
+            imgElement.classList.add('img-fluid', 'insights-image');
+
+            row1.appendChild(firstImgDiv);
+            firstImgDiv.appendChild(insightsWrapper1);
+            insightsWrapper1.appendChild(imgElement);
+
+        } else if (index==1){
+
+        }
     })
-    
+    console.log(interestsMain);
     // // Create section element with class "home-interests-wraper"
     // const section = document.createElement('section');
     // section.classList.add('home-interests-wraper');
