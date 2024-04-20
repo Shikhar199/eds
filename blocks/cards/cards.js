933cf87363459a8e9dce4380c5e16706b59a1ba5
Shikhar199 <74426605+Shikhar199@users.noreply.github.com>
2024-04-20 18:19:33 +0530

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,7 +1,6 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
-  console.log("Hi");
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
