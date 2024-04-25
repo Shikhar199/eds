c6940137c61b9e905cb63bf852abdcec2dfe0b55
Shikhar <shikharsharma612@gmail.com>
2024-04-25 16:32:04 +0530

    bbb

--- blocks/slider/slider.js
+++ blocks/slider/slider.js
@@ -108,7 +108,7 @@ function createSlickItem(row,r){
                 } else if(i==9){
                     cardBodyAnchorElement.setAttribute("href", node.textContent.trim())
                     var selectorString = 'div[data-slick-index="' + r + '"]';
-                    console.log(document.querySelector(".slick-track").querySelector(selectorString));
+                    console.log(document.querySelector(".slick-track"));
                 }
             })
             popularCardDiv.appendChild(cardBodyDiv);
