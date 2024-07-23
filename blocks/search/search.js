d0f12559a5a8c804785e03ff25a637cddcf339fd
Shikhar <shikhar.sharma@axeno.co>
2024-07-23 14:17:48 +0530

    ...

--- blocks/search/search.js
+++ blocks/search/search.js
@@ -1,3 +1,8 @@
 export default function decorate(block){
     console.log(block);
+    [...block.children].forEach((row)=>
+        [row.children].forEach((col)=>{
+            console.log(col.innerText());
+        })
+    )
 }
